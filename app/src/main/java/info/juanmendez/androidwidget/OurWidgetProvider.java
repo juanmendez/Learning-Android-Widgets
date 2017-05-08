818c287090b2af51ea06bca0e1e59658f5f2225d
Juan Mendez <contact@juanmendez.info>
2017-05-08 18:27:02 -0500

    changes reflect updating a single widget

--- app/src/main/java/info/juanmendez/androidwidget/OurWidgetProvider.java
+++ app/src/main/java/info/juanmendez/androidwidget/OurWidgetProvider.java
@@ -1,49 +1,69 @@
 package info.juanmendez.androidwidget;
+
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+
 import java.util.Random;
+
 import timber.log.Timber;
+
 
 /**
  * Created by Juan Mendez on 5/8/2017.
  * www.juanmendez.info
  * contact@juanmendez.info
- *
- * Upon updating one widget, you get all other instance display the same string value
  */
+
 public class OurWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() == null) {
+
+            updateWidget(context,
+                    AppWidgetManager.getInstance(context),
+                    intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1));
+        }
+        else {
+            super.onReceive(context, intent);
+        }
+    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         Timber.i( "on update" );
         final int count = appWidgetIds.length;
 
-        int randomInt = (new Random().nextInt(900) + 100);
         for (int i = 0; i < count; i++) {
-            updateWidget( context, appWidgetManager, appWidgetIds[i], appWidgetIds, randomInt );
+            updateWidget( context, appWidgetManager, appWidgetIds[i] );
         }
     }
 
-    private void updateWidget( Context ctxt, AppWidgetManager mgr, int appWidgetId, int[] appWidgetIds, int randomInt ){
 
-        String randomString = String.format("%03d", randomInt );
+    private void updateWidget( Context ctxt, AppWidgetManager mgr, int appWidgetId ){
 
-        Timber.i( appWidgetId + ": change for textview " + randomString );
-        RemoteViews updateViews = new RemoteViews(ctxt.getPackageName(), R.layout.widget_demo);
-        updateViews.setTextViewText(R.id.textView, randomString);
+        if( appWidgetId > 0 ){
 
-        Intent intent = new Intent(ctxt, OurWidgetProvider.class);
-        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds );
+            int randomInt = (new Random().nextInt(900) + 100);
 
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(ctxt,
-                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        updateViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            String randomNumber = String.format("%03d", randomInt );
 
-        mgr.updateAppWidget(appWidgetId, updateViews);
+            RemoteViews updateViews = new RemoteViews(ctxt.getPackageName(), R.layout.widget_demo);
+            updateViews.setTextViewText(R.id.textView, randomNumber);
+            Timber.i( appWidgetId + ": change for textview " + randomNumber );
+
+            Intent intent = new Intent(ctxt, OurWidgetProvider.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId );
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(ctxt,
+                    appWidgetId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            updateViews.setOnClickPendingIntent(R.id.actionButton, pendingIntent);
+            mgr.updateAppWidget(appWidgetId, updateViews);
+
+        }
     }
 }
