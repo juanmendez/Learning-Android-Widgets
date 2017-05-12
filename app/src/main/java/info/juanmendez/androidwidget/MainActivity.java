f7ee5d1ca886665daf214009d0d713b4cdb77a6c
Juan Mendez <contact@juanmendez.info>
2017-05-12 01:34:58 -0500

    show toast

--- app/src/main/java/info/juanmendez/androidwidget/MainActivity.java
+++ app/src/main/java/info/juanmendez/androidwidget/MainActivity.java
@@ -2,6 +2,7 @@ package info.juanmendez.androidwidget;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String word=getIntent().getStringExtra(OurWidgetProvider.EXTRA_WORD);
+
+        if (word != null) {
+            Toast.makeText(this, "received: " + word, Toast.LENGTH_LONG ).show();
+        }
     }
 }
