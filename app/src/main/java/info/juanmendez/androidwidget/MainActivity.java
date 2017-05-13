0b7d18eadcaff03f4333f1df705639a2767d8a4e
Juan Mendez <contact@juanmendez.info>
2017-05-13 00:39:36 -0500

    onStart() should be used to display latest intent's string extra

--- app/src/main/java/info/juanmendez/androidwidget/MainActivity.java
+++ app/src/main/java/info/juanmendez/androidwidget/MainActivity.java
@@ -10,7 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
         String word=getIntent().getStringExtra(OurWidgetProvider.EXTRA_WORD);
 
         if (word != null) {
